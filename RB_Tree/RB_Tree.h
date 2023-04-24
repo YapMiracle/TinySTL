339e468ef3cb69792ddc188182ee4ab7425c65d0
Allen Mirac <2049829115@qq.com>
2023-04-24 19:46:15 +0800

    :fire:格式化文档

--- RB_Tree/RB_Tree.h
+++ RB_Tree/RB_Tree.h
@@ -5,92 +5,112 @@
 #include <iostream>
 #include <queue>
 
-template<class T>
-class RB_Tree{
+template <class T>
+class RB_Tree
+{
 public:
-    RB_Tree(T Root_data);
-    ~RB_Tree();
-    void Left_Rotate(RB_Tree_Node<T> * current_node);
-    void Right_Rotate(RB_Tree_Node<T>* current_node);
+	RB_Tree(T Root_data);
+	~RB_Tree();
+	void Left_Rotate(RB_Tree_Node<T> *current_node);
+	void Right_Rotate(RB_Tree_Node<T> *current_node);
 	void Insert_Node(T insert_data);
 	int Delete_Node(T delete_data);
-	void Fix_Tree(RB_Tree_Node<T>* current_node);
-	void erase_Node(RB_Tree_Node<T>* Node_del);
-	void Fix_Tree_Delete(RB_Tree_Node<T>* current_node);
-	RB_Tree_Node<T>* Find_Successor_Node(RB_Tree_Node<T>* current_node);
+	void Fix_Tree(RB_Tree_Node<T> *current_node);
+	void erase_Node(RB_Tree_Node<T> *Node_del);
+	void Fix_Tree_Delete(RB_Tree_Node<T> *current_node);
+	RB_Tree_Node<T> *Find_Successor_Node(RB_Tree_Node<T> *current_node);
 	void display();
-	RB_Tree_Node<T>* Root_Node;
+	RB_Tree_Node<T> *Root_Node;
 };
 
 template <class T>
-RB_Tree<T>::RB_Tree(T Root_Data):Root_Node(nullptr){
-    if(Root_Node == nullptr){
-        Root_Node = new RB_Tree_Node<T>(Root_Data);
-        Root_Node->color_tag = 0;
-    }
+RB_Tree<T>::RB_Tree(T Root_Data) : Root_Node(nullptr)
+{
+	if (Root_Node == nullptr)
+	{
+		Root_Node = new RB_Tree_Node<T>(Root_Data);
+		Root_Node->color_tag = 0;
+	}
 }
 
 template <class T>
-RB_Tree<T>::~RB_Tree(){
-
+RB_Tree<T>::~RB_Tree()
+{
 }
 
 template <class T>
-void RB_Tree<T>::display(){
-    RB_Tree_Node<T> *temp_node = Root_Node;
-    std::queue<RB_Tree_Node<T>*> q;
+void RB_Tree<T>::display()
+{
+	RB_Tree_Node<T> *temp_node = Root_Node;
+	std::queue<RB_Tree_Node<T> *> q;
 	q.push(temp_node);
-    while(!q.empty()){
+	while (!q.empty())
+	{
 		temp_node = q.front();
-        std::cout<<" "<<temp_node->data<<std::endl;
+		std::cout << " " << temp_node->data << std::endl;
 		q.pop();
-        if(temp_node->left_child!=nullptr){
-            q.push(temp_node->left_child);
-            continue;
-        }
-        if(temp_node->right_child!=nullptr){
-            q.push(temp_node->right_child);
-            continue;
-        }
-    }
+		if (temp_node->left_child != nullptr)
+		{
+			q.push(temp_node->left_child);
+			continue;
+		}
+		if (temp_node->right_child != nullptr)
+		{
+			q.push(temp_node->right_child);
+			continue;
+		}
+	}
 }
 template <class T>
-void RB_Tree<T>::Insert_Node(T insert_data){
-    RB_Tree_Node<T> *temp_node = Root_Node;
-    while(temp_node != nullptr){
-        if(insert_data > temp_node->data){
-            if(temp_node->right_child == nullptr){
-                temp_node->right_child = new RB_Tree_Node<T>(insert_data);
-                temp_node->right_child->color_tag = 1;
-                temp_node->father_node = temp_node;
-                if(temp_node->color_tag == 1){
-                    Fix_Tree(temp_node->right_child);
-                }
-                break;
-            }else {
-                temp_node = temp_node->right_child;
-            }
-        }else{
-			if (temp_node->left_child == NULL){
-				temp_node->left_child  = new RB_Tree_Node<T>(insert_data);
+void RB_Tree<T>::Insert_Node(T insert_data)
+{
+	RB_Tree_Node<T> *temp_node = Root_Node;
+	while (temp_node != nullptr)
+	{
+		if (insert_data > temp_node->data)
+		{
+			if (temp_node->right_child == nullptr)
+			{
+				temp_node->right_child = new RB_Tree_Node<T>(insert_data);
+				temp_node->right_child->color_tag = 1;
+				temp_node->father_node = temp_node;
+				if (temp_node->color_tag == 1)
+				{
+					Fix_Tree(temp_node->right_child);
+				}
+				break;
+			}
+			else
+			{
+				temp_node = temp_node->right_child;
+			}
+		}
+		else
+		{
+			if (temp_node->left_child == NULL)
+			{
+				temp_node->left_child = new RB_Tree_Node<T>(insert_data);
 				temp_node->left_child->color_tag = 1;
 				temp_node->left_child->father_node = temp_node;
-				if (temp_node->color_tag == 1){
+				if (temp_node->color_tag == 1)
+				{
 					Fix_Tree(temp_node->left_child);
-				}				
+				}
 				break;
-			}else{
+			}
+			else
+			{
 				temp_node = temp_node->left_child;
-			}			
+			}
 		}
-    }
+	}
 }
 
-template<class T>
+template <class T>
 int RB_Tree<T>::Delete_Node(T delete_data)
-{	
-	RB_Tree_Node<T>* temp_Node = Root_Node;
-	while(temp_Node->data != delete_data && temp_Node != NULL)
+{
+	RB_Tree_Node<T> *temp_Node = Root_Node;
+	while (temp_Node->data != delete_data && temp_Node != NULL)
 	{
 		if (delete_data > temp_Node->data)
 		{
@@ -100,77 +120,71 @@ int RB_Tree<T>::Delete_Node(T delete_data)
 		{
 			temp_Node = temp_Node->left_child;
 		}
-	}	
+	}
 	if (temp_Node)
-	//找到返回的数据
+	// 找到返回的数据
 	{
 		int color_tag = temp_Node->color_tag;
-		
+
 		if (temp_Node->left_child == NULL && temp_Node->right_child == NULL)
-		//左右子树为空则直接删除
+		// 左右子树为空则直接删除
 		{
-			//			
+			//
 			delete temp_Node;
 		}
-		else
-		if (temp_Node->left_child == NULL && temp_Node->right_child != NULL)
-		//左子树为空,右子树不为空
-		{				
+		else if (temp_Node->left_child == NULL && temp_Node->right_child != NULL)
+		// 左子树为空,右子树不为空
+		{
 			if (temp_Node != Root_Node)
-			//不为根节点
+			// 不为根节点
 			{
 				if (temp_Node->Father_Node->left_child == temp_Node)
 				{
 					temp_Node->Father_Node->left_child = temp_Node->right_child;
 					temp_Node->right_child->Father_Node = temp_Node->Father_Node;
-					
 				}
 				else
 				{
 					temp_Node->Father_Node->right_child = temp_Node->right_child;
 					temp_Node->right_child->Father_Node = temp_Node->Father_Node;
-					
 				}
 			}
 			else
-			//根节点
+			// 根节点
 			{
 				Root_Node = temp_Node->right_child;
 				temp_Node->right_child->Father_Node = Root_Node;
-				
 			}
 		}
 		else if (temp_Node->left_child != NULL && temp_Node->right_child == NULL)
-		//左子树不为空,右子树为空
+		// 左子树不为空,右子树为空
 		{
 			if (temp_Node != Root_Node)
-				//不为根节点
+			// 不为根节点
 			{
 				if (temp_Node->Father_Node->left_child == temp_Node)
 				{
 					temp_Node->Father_Node->left_child = temp_Node->left_child;
 					temp_Node->left_child->Father_Node = temp_Node->Father_Node;
-					
 				}
 				else
 				{
 					temp_Node->Father_Node->right_child = temp_Node->left_child;
 					temp_Node->left_child->Father_Node = temp_Node->Father_Node;
-					
 				}
 			}
 			else
-				//根节点
+			// 根节点
 			{
 				Root_Node = temp_Node->left_child;
 				temp_Node->left_child->Father_Node = Root_Node;
-				//delete temp_Node;
+				// delete temp_Node;
 			}
 		}
 		else if (temp_Node->left_child != NULL && temp_Node->right_child != NULL)
-		//左右子树都不为空
+		// 左右子树都不为空
 		{
-			RB_Tree_Node<T>* Successor_Node = Find_Successor_Node(temp_Node);
+			RB_Tree_Node<T> *Successor_Node = Find_Successor_Node(temp_Node);
 			if (temp_Node == temp_Node->Father_Node->left_child)
 			{
 				temp_Node->Father_Node->left_child = Successor_Node;
@@ -183,7 +197,6 @@ int RB_Tree<T>::Delete_Node(T delete_data)
 					Successor_Node->Father_Node->left_child = Successor_Node->right_child;
 					Successor_Node->right_child->Father_Node = Successor_Node->Father_Node;
 				}
-				
 			}
 			else
 			{
@@ -197,86 +210,85 @@ int RB_Tree<T>::Delete_Node(T delete_data)
 					Successor_Node->Father_Node->left_child = Successor_Node->right_child;
 					Successor_Node->right_child->Father_Node = Successor_Node->Father_Node;
 				}
-			}			
+			}
 		}
-		
+
 		if (color_tag == 0)
-		//如果删除的节点为黑色  则需调整红黑树
+		// 如果删除的节点为黑色  则需调整红黑树
 		{
 			Fix_Tree_Delete(temp_Node);
 		}
 	}
-	//未发现删除的数据  返回-1
+	// 未发现删除的数据  返回-1
 	else
 	{
 		return -1;
 	}
 	return 1;
-	
 }
 
 template <class T>
-void RB_Tree<T>::Fix_Tree(RB_Tree_Node<T>* current_Node)
+void RB_Tree<T>::Fix_Tree(RB_Tree_Node<T> *current_Node)
 {
-	RB_Tree_Node<T>* temp_current_Node = current_Node;	
-	RB_Tree_Node<T>* uncle_Node;	
-		
-	while(true)
+	RB_Tree_Node<T> *temp_current_Node = current_Node;
+	RB_Tree_Node<T> *uncle_Node;
+
+	while (true)
 	{
-		if ( temp_current_Node->father_node == NULL)
+		if (temp_current_Node->father_node == NULL)
 		{
 			break;
 		}
 		if (temp_current_Node->father_node->color_tag != 1)
 		{
 			break;
-		}		 
-		RB_Tree_Node<T>* father_node = temp_current_Node->father_node;
-		RB_Tree_Node<T>* grandfa_Node = father_node->father_node;
+		}
+		RB_Tree_Node<T> *father_node = temp_current_Node->father_node;
+		RB_Tree_Node<T> *grandfa_Node = father_node->father_node;
 		if (grandfa_Node)
 		{
 			if (father_node == grandfa_Node->left_child)
 			{
 				uncle_Node = grandfa_Node->right_child;
-				//如果有叔叔节点时
+				// 如果有叔叔节点时
 				if (uncle_Node)
 				{
-					//情况1 叔叔为红色  将父亲节点和叔叔节点设置为黑色 
-					//祖父节点设置为红色 将祖父节点设置为当前节点
+					// 情况1 叔叔为红色  将父亲节点和叔叔节点设置为黑色
+					// 祖父节点设置为红色 将祖父节点设置为当前节点
 					if (uncle_Node->color_tag == 1)
 					{
 						uncle_Node->color_tag = 0;
-						father_node->color_tag = 0;						
-						grandfa_Node->color_tag = 1;						
-						temp_current_Node = grandfa_Node;						
+						father_node->color_tag = 0;
+						grandfa_Node->color_tag = 1;
+						temp_current_Node = grandfa_Node;
 					}
-					//情况2：叔叔是黑色 且当前节点为右孩子 将父节点作为当前节点 对父节点进行左旋
+					// 情况2：叔叔是黑色 且当前节点为右孩子 将父节点作为当前节点 对父节点进行左旋
 					else if (temp_current_Node == father_node->right_child)
 					{
-						temp_current_Node = temp_current_Node->father_node;						
-						Left_Rotate(temp_current_Node);					
+						temp_current_Node = temp_current_Node->father_node;
+						Left_Rotate(temp_current_Node);
 					}
-					//情况3：叔叔是黑色 且当前节点为左孩子 将父节点设为黑色 祖父节点设为红色 对祖父节点右旋
+					// 情况3：叔叔是黑色 且当前节点为左孩子 将父节点设为黑色 祖父节点设为红色 对祖父节点右旋
 					else
 					{
 						father_node->color_tag = 0;
 						grandfa_Node->color_tag = 1;
-						Right_Rotate(grandfa_Node);				
-					}					
+						Right_Rotate(grandfa_Node);
+					}
 				}
-				//没有叔叔节点时
+				// 没有叔叔节点时
 				else
 				{
 					if (temp_current_Node == father_node->right_child)
 					{
-						temp_current_Node = temp_current_Node->father_node;						
-						Left_Rotate(temp_current_Node);					
-					}					
+						temp_current_Node = temp_current_Node->father_node;
+						Left_Rotate(temp_current_Node);
+					}
 					else
 					{
 						father_node->color_tag = 0;
 						grandfa_Node->color_tag = 1;
-						Right_Rotate(grandfa_Node);	
+						Right_Rotate(grandfa_Node);
 					}
 				}
 			}
@@ -285,75 +297,78 @@ void RB_Tree<T>::Fix_Tree(RB_Tree_Node<T>* current_Node)
 				uncle_Node = grandfa_Node->left_child;
 				if (uncle_Node)
 				{
-					//情况1 叔叔为红色  将父亲节点和叔叔节点设置为黑色 
-					//祖父节点设置为红色 将祖父节点设置为当前节点
+					// 情况1 叔叔为红色  将父亲节点和叔叔节点设置为黑色
+					// 祖父节点设置为红色 将祖父节点设置为当前节点
 					if (uncle_Node->color_tag == 1)
 					{
 						uncle_Node->color_tag = 0;
-						father_node->color_tag = 0;						
-						grandfa_Node->color_tag = 1;						
-						temp_current_Node = grandfa_Node;						
+						father_node->color_tag = 0;
+						grandfa_Node->color_tag = 1;
+						temp_current_Node = grandfa_Node;
 					}
-					//情况2：叔叔是黑色 且当前节点为右孩子 将父节点作为当前节点 对父节点进行左旋
+					// 情况2：叔叔是黑色 且当前节点为右孩子 将父节点作为当前节点 对父节点进行左旋
 					else if (temp_current_Node == father_node->left_child)
 					{
 						temp_current_Node = temp_current_Node->father_node;
-						Right_Rotate(temp_current_Node);						
+						Right_Rotate(temp_current_Node);
 					}
-					//情况3：叔叔是黑色 且当前节点为左孩子 将父节点设为黑色 祖父节点设为红色 对祖父节点右旋
+					// 情况3：叔叔是黑色 且当前节点为左孩子 将父节点设为黑色 祖父节点设为红色 对祖父节点右旋
 					else
 					{
 						father_node->color_tag = 0;
 						grandfa_Node->color_tag = 1;
-						Left_Rotate(grandfa_Node);	
-					}									
+						Left_Rotate(grandfa_Node);
+					}
 				}
 				else
 				{
 					if (temp_current_Node == father_node->left_child)
 					{
 						temp_current_Node = temp_current_Node->father_node;
-						Right_Rotate(temp_current_Node);						
-					}					
+						Right_Rotate(temp_current_Node);
+					}
 					else
 					{
 						father_node->color_tag = 0;
 						grandfa_Node->color_tag = 1;
-						Left_Rotate(grandfa_Node);	
-					}	
+						Left_Rotate(grandfa_Node);
+					}
 				}
 			}
 		}
 	}
 	Root_Node->color_tag = 0;
-
 }
 
 template <class T>
-void RB_Tree<T>::Left_Rotate(RB_Tree_Node<T> * current_node)
+void RB_Tree<T>::Left_Rotate(RB_Tree_Node<T> *current_node)
 {
-	RB_Tree_Node<T>* Right_child = current_node->right_child;	
-	RB_Tree_Node<T>* Father_node = current_node->father_node;
+	RB_Tree_Node<T> *Right_child = current_node->right_child;
+	RB_Tree_Node<T> *Father_node = current_node->father_node;
 	current_node->right_child = Right_child->left_child;
 	Right_child->father_node = Father_node;
-	if (Father_node == NULL){
+	if (Father_node == NULL)
+	{
 		Root_Node = Right_child;
-	}else if(current_node == Father_node->left_child){
+	}
+	else if (current_node == Father_node->left_child)
+	{
 		Father_node->left_child = Right_child;
-	}else{
+	}
+	else
+	{
 		Father_node->right_child = Right_child;
-	}	
+	}
 
 	Right_child->left_child = current_node;
 	current_node->father_node = Right_child;
-
 }
 
 template <class T>
-void RB_Tree<T>::Right_Rotate(RB_Tree_Node<T>* current_node)
+void RB_Tree<T>::Right_Rotate(RB_Tree_Node<T> *current_node)
 {
-	RB_Tree_Node<T>* left_Node = current_node->left_child;
-	RB_Tree_Node<T>* Father_node = current_node->father_node;
+	RB_Tree_Node<T> *left_Node = current_node->left_child;
+	RB_Tree_Node<T> *Father_node = current_node->father_node;
 	current_node->left_child = left_Node->right_child;
 	left_Node->right_child = current_node;
 	if (Father_node == NULL)
@@ -372,8 +387,8 @@ void RB_Tree<T>::Right_Rotate(RB_Tree_Node<T>* current_node)
 	left_Node->father_node = Father_node;
 }
 
-template<class T>
-void RB_Tree<T>::erase_Node(RB_Tree_Node<T>* Node_del)
+template <class T>
+void RB_Tree<T>::erase_Node(RB_Tree_Node<T> *Node_del)
 {
 	if (Node_del->father_node)
 	{
@@ -383,7 +398,7 @@ void RB_Tree<T>::erase_Node(RB_Tree_Node<T>* Node_del)
 	Node_del->father_node = NULL;
 	Node_del->left_child = NULL;
 	Node_del->Right_child = NULL;
-	Node_del->data = NULL;	
+	Node_del->data = NULL;
 	delete Node_del;
 }
 #endif // RB_TREE_H
--- RB_Tree/RB_Tree_Node.h
+++ RB_Tree/RB_Tree_Node.h
@@ -1,8 +1,9 @@
 #ifndef RB_TREE_NODE_H
 #define RB_TREE_NODE_H
 
-template<class T>
-class RB_Tree_Node{
+template <class T>
+class RB_Tree_Node
+{
 public:
     RB_Tree_Node(T data_in);
     ~RB_Tree_Node(void);
@@ -10,16 +11,17 @@ public:
     RB_Tree_Node *right_child;
     RB_Tree_Node *father_node;
     T data;
-    int color_tag; // 1 hong 0 hei
+    int color_tag; // 1 Red 0 Black
 };
 template <typename T>
-RB_Tree_Node<T>::RB_Tree_Node(T data_in):data(data_in), color_tag(0), 
-                left_child(nullptr), right_child(nullptr), father_node(nullptr){
-                    data = data_in;
-                    color_tag = 1;
+RB_Tree_Node<T>::RB_Tree_Node(T data_in) : data(data_in), color_tag(0),
+                                           left_child(nullptr), right_child(nullptr), father_node(nullptr)
+{
+    data = data_in;
+    color_tag = 1;
 }
 template <typename T>
-RB_Tree_Node<T>::~RB_Tree_Node(void){
-    
+RB_Tree_Node<T>::~RB_Tree_Node(void)
+{
 }
 #endif // RB_TREE_NODE_H
